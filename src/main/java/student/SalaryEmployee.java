0ef484b2dc822ab00a200f8e2432b4d6ff3e27e6
hewen <dreamershim@163.com>
2025-02-05 00:55:19 -0800

    upload hw

--- src/main/java/student/SalaryEmployee.java
+++ src/main/java/student/SalaryEmployee.java
@@ -212,6 +212,6 @@ public class SalaryEmployee extends EmployeeGrossPay implements IEmployee {
     @Override
     protected BigDecimal calculateGrossPay(double hoursWorked) {
         BigDecimal payRateBD = BigDecimal.valueOf(payRate);
-        return payRateBD.divide(BigDecimal.valueOf(TIMES), RoundingMode.HALF_UP);
+        return payRateBD.divide(BigDecimal.valueOf(TIMES), RoundingMode.UP);
     }
 }
